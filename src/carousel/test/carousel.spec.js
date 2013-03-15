9c74688521fd87918faad1b08f8cf4b57a7aa56b
Matt Raible <matt@raibledesigns.com>
2013-03-15 16:36:55 -0600

    Added test to verify navigation shows when more than one slide.

--- src/carousel/test/carousel.spec.js
+++ src/carousel/test/carousel.spec.js
@@ -65,23 +65,34 @@ describe('carousel', function() {
     });
     
     it('should hide navigation when only one slide', function () {
-      scope.slides=[];
+      scope.slides=[{active:false,content:'one'}];
       scope.$apply();
       elm = $compile(
           '<carousel interval="interval" no-transition="true">' +
             '<slide ng-repeat="slide in slides" active="slide.active">' +
               '{{slide.content}}' +
             '</slide>' +
-          '</carousel>'
+          '</carousel>' 
         )(scope);
       var indicators = elm.find('ol.carousel-indicators > li');
       expect(indicators.length).toBe(0);
-
+      
       var navNext = elm.find('a.right');
       expect(navNext.length).toBe(0);
-
+      
       var navPrev = elm.find('a.left');
       expect(navPrev.length).toBe(0);
+    });
+    
+    it('should show navigation when there are 3 slides', function () {  
+      var indicators = elm.find('ol.carousel-indicators > li');
+      expect(indicators.length).not.toBe(0);
+      
+      var navNext = elm.find('a.right');
+      expect(navNext.length).not.toBe(0);
+      
+      var navPrev = elm.find('a.left');
+      expect(navPrev.length).not.toBe(0);
     });
 
     it('should go to next when clicking next button', function() {
